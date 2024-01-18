f476ff3f66093a0696d73a5f8cb7fe234d2d81c5
Samind2 <111554627+Samind2@users.noreply.github.com>
2024-01-18 16:51:29 +0700

    Add files via upload

---
+++ TestGooglwebsite/src/MultiBrower.java
@@ -0,0 +1,102 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+@SuppressWarnings("unused")
+class MultiBrower {
+
+	@Test
+	void testSearchByKeyword() {
+        WebDriver driver = null;
+        System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        
+        WebElement search_box = driver.findElement(By.name("q"));
+        search_box.sendKeys("NPRU");
+        search_box.sendKeys(Keys.ENTER);
+        
+        String result = driver.findElement(By.className("VuuXrf")).getText();
+        //System.out.print("Result :"+result);
+        assertEquals("มหาวิทยาลัยราชภัฏนครปฐม",result);
+        driver.close();
+	}
+	
+	@Test
+	void testSearchByKeyword2() {
+        WebDriver driver = null;
+        System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        
+        WebElement search_box = driver.findElement(By.name("q"));
+        search_box.sendKeys("KRU");
+        search_box.sendKeys(Keys.ENTER);
+        
+        String result = driver.findElement(By.className("VuuXrf")).getText();
+        //System.out.print("Result :"+result);
+        assertEquals("Kanchanaburi Rajabhat University",result);
+        driver.close();
+	}
+	
+	@Test
+
+	void test3() {
+
+	WebDriver driver = null;
+
+	System.setProperty("webdriver.edge.driver","D:\\webdriver\\msedgedriver.exe");
+
+	driver = new EdgeDriver();
+
+	driver.get("https://www.google.com");
+
+	WebElement search_box = driver.findElement(By.name("q"));
+
+	search_box.sendKeys("KRU");
+
+	search_box.sendKeys(Keys.ENTER);
+
+	String result = driver.findElement(By.className("VuuXrf")).getText();
+
+	assertEquals("Kanchanaburi Rajabhat University", result);
+
+	driver.close();
+
+	}
+	
+	@Test
+	
+	void test4() {
+
+		WebDriver driver = null;
+
+		System.setProperty("webdriver.gecko.driver","D:\\webdriver\\geckodriver.exe");
+
+		driver = new FirefoxDriver();
+
+		driver.get("https://www.google.com");
+
+		WebElement search_box = driver.findElement(By.name("q"));
+
+		search_box.sendKeys("PBRU");
+
+		search_box.sendKeys(Keys.ENTER);
+
+		String title = driver.getTitle();
+
+		assertEquals("Google", title);
+
+		driver.close();
+
+		}
+
+}
